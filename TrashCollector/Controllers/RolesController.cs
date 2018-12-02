8ce0f8e4b13254a06ef999568d36941f576780b2
Brian Pennington <srsrec@gmail.com>
2018-12-02 09:15:54 -0600

    Created RolesController

---
+++ TrashCollector/Controllers/RolesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class RolesController : Controller
+    {
+        public ApplicationDbContext db;
+
+        public RolesController()
+        {
+            db = new ApplicationDbContext();
+        }
+        
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+            return View(roles);
+        }
+
+        public ActionResult Create()
+        {
+            IdentityRole role = new IdentityRole();
+            return View(role);
+        }
+
+        public ActionResult Create(IdentityRole role)
+        {
+            db.Roles.Add(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
